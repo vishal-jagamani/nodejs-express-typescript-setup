4ca6cba65ae72d61c58f3f844c944a289ab4f821
Vishal Jagamani <vishaljagamani20@gmail.com>
2025-03-10 17:40:53 +0530

    Basic Setup

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from 'express';
+import { ENABLE_AUTH } from '../config/config';
+
+const noAuth = (req: Request, res: Response, next: NextFunction) => {
+    next();
+};
+
+const auth = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) res.status(401).send('Unauthorized');
+        else {
+            // Implement auth logic
+            next();
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+        res.status(401).send('Unauthorized');
+    }
+};
+
+export const authMiddleware = () => {
+    return ENABLE_AUTH === 'true' ? auth : noAuth;
+};
